e6fa2d2a7eacd715fcf8011c663d3054b3d5e83b
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-05-05 14:18:24 +0200

    First commit

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          <link rel="icon" href="/vercel.svg" />
+          <link
+            rel="stylesheet"
+            href="https://use.typekit.net/egi6zfi.css"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
