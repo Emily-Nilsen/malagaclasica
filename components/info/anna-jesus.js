d269d209fe276c86874e3f0d401540be861a1695
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-05-08 14:50:05 +0200

    Change img tags

--- components/info/anna-jesus.js
+++ components/info/anna-jesus.js
@@ -66,30 +66,42 @@ export default function AnnaJesus() {
 
             <div>
               <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden">
-                <div>
-                  <img
+                <div className="w-full h-full object-center object-cover">
+                  <Image
                     src="/static/crescendo-img-12-blue.webp"
-                    alt="Black kettle with long pour spot and angled body on marble counter next to coffee mug and pour-over system."
-                    className="w-full h-full object-center object-cover"
+                    alt="Málaga Clásica"
+                    width={1000}
+                    height={1000}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="top"
                   />
                 </div>
               </div>
               <div className="grid grid-cols-2 gap-4 mt-4 sm:gap-6 sm:mt-6 lg:gap-8 lg:mt-8">
                 <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden">
-                  <div>
-                    <img
+                  <div className="w-full h-full object-center object-cover">
+                    <Image
                       src="/static/jesus-blue.webp"
-                      alt="Detail of temperature setting button on kettle bass with digital degree readout."
-                      className="w-full h-full object-center object-cover"
+                      alt="Jesús Reina"
+                      width={1000}
+                      height={1000}
+                      layout="fill"
+                      objectFit="cover"
+                      objectPosition="center"
                     />
                   </div>
                 </div>
                 <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden">
-                  <div>
-                    <img
+                  <div className="w-full h-full object-center object-cover">
+                    <Image
                       src="/static/anna-blue.webp"
-                      alt="Kettle spout pouring boiling water into coffee grounds in pour-over mug."
-                      className="w-full h-full object-center object-cover"
+                      alt="Anna Nilsen"
+                      width={1000}
+                      height={1000}
+                      layout="fill"
+                      objectFit="cover"
+                      objectPosition="center"
                     />
                   </div>
                 </div>
--- locales/en/info.js
+++ locales/en/info.js
@@ -4,7 +4,7 @@ const info = {
   para_2: `In 'Crescendo', we will symbolically show the festival's growth during these last ten years. We represent each of the ten years by the number of artists that form the chamber ensembles.`,
   para_3: `Each musical performance tells the story of Málaga Clásica through much-loved works and through new sounds that will captivate them. To the public's delight, the opening gala will feature works by Beethoven and Dvorak. Then, each concert will 'crescendo' in terms of formations, from one instrument to ten.`,
   subtitle: `The composers`,
-  para_4: `The festival includes composers such as Bach, Cassadó, Borodin, and Schuman. Finally, the deceit by DuBois will bring the thematic concerts to a close. Referential artists from diverse countries such as Russia, Ukraine, Germany, Norway, Canada, Bulgaria, Romania, and Spain will join each other for extraordinary performances.`,
+  para_4: `The festival includes composers such as Bach, Cassadó, Borodin, and Schuman. Finally, the decet by Dubois will bring the thematic concerts to a close. Referential artists from diverse countries such as Russia, Ukraine, Germany, Norway, Canada, Bulgaria, Romania, and Spain will join each other for extraordinary performances.`,
   para_5: `As a coda and with a view to the future, Teatro Cervantes will host the Closing Gala. This concert, which follows the thematic material into chamber music on a large scale, manifests Málaga Clásica's wish for these ten years to be the first incredible years of many.`,
   para_6: `It will take us from 'crescendo' to 'fortísimo súbito', from ten musicians to one, and by one of the most outstanding symphony orchestras in Spain, the Franz Schubert Filharmonia.`,
   para_7: `With all the love and music from our hearts,`,
