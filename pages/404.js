17d5f48d2c7319f8a25a6e0df5482a62c1fbee06
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-05-17 17:19:35 +0200

    Delete layout wrapper from success and 404

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Layout from '../../components/layout';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
@@ -9,29 +8,23 @@ export default function PageNotFound() {
   const { t } = useTranslation();
 
   return (
-    <Layout
-      title={t('common:notfound_title')}
-      description={t('common:notfound_description')}
-      keywords={t('common:notfound_keywords')}
-    >
-      <main className="h-[50vh] bg-top bg-cover bg-notfound sm:bg-top">
-        <div className="flex flex-col items-center justify-center h-full">
-          <h1 className="text-4xl font-bold text-center md:text-6xl text-mossCrescendo">
-            {t('common:notfound_title_1')}
-            <span className="block">{t('common:notfound_title_2')}</span>
-          </h1>
-          <p className="pt-2 text-center text-md text-coalCrescendo sm:pt-4">
-            {t('common:notfound_text')}
-          </p>
-          <div className="mt-6">
-            <Link href="/" passHref>
-              <button className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white transition duration-200 ease-in-out border border-transparent rounded-md shadow-none bg-mossCrescendo hover:bg-blueCrescendo focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blueCrescendo">
-                {t('common:button_back')}
-              </button>
-            </Link>
-          </div>
+    <main className="h-[50vh] bg-top bg-cover bg-notfound sm:bg-top">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold text-center md:text-6xl text-mossCrescendo">
+          {t('common:notfound_title_1')}
+          <span className="block">{t('common:notfound_title_2')}</span>
+        </h1>
+        <p className="pt-2 text-center text-md text-coalCrescendo sm:pt-4">
+          {t('common:notfound_text')}
+        </p>
+        <div className="mt-6">
+          <Link href="/" passHref>
+            <button className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white transition duration-200 ease-in-out border border-transparent rounded-md shadow-none bg-mossCrescendo hover:bg-blueCrescendo focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blueCrescendo">
+              {t('common:button_back')}
+            </button>
+          </Link>
         </div>
-      </main>
-    </Layout>
+      </div>
+    </main>
   );
 }
