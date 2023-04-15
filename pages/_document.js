72abc60fb42fb080a16e7b400de38601b9b0e530
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-04-15 09:49:34 +0200

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,10 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          <link rel="icon" href="/crescendo-grey.svg" />
+          <link
+            rel="icon"
+            href="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/M%C3%A1laga%20Cl%C3%A1sica/favicon_qyejeq.svg"
+          />
           <link
             rel="stylesheet"
             href="https://use.typekit.net/zgi1gdg.css"
